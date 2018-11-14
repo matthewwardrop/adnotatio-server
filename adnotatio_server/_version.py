6e3f41266b51e707439a360644d0c5eb4e3cd5c1
Matthew Wardrop <mawardrop@netflix.com>
2018-11-13 22:53:48 -0800

    Bump version to 0.1.1

--- adnotatio_server/_version.py
+++ adnotatio_server/_version.py
@@ -5,7 +5,7 @@ __all__ = [
 
 __author__ = "Matthew Wardrop"
 __author_email__ = "mpwardrop@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __git_uri__ = "https://github.com/matthewwardrop/adnotatio-server.git"
 
 
